57698221a12a03af775aa9706463446ecdb983fc
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2023-11-29 01:48:03 -0300

    adjusts main page alignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex min-h-screen flex-col justify-between p-24 bg-primary-content">
-        <section className="max-w-2xl">
-          <h1>Test</h1>
+      <main className="flex min-h-screen flex-col justify-between p-24 bg-primary-content items-center">
+        <section className="flex w-full max-w-2xl px-4">
+          <h1>Hi, I'm Guilherme!</h1>
         </section>
       </main>
     </>
